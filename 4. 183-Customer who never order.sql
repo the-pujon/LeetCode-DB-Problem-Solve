1158a35dbe1714ea90c6ab5110653d7f494b7410
Pujon Das Auvi <auvipujondas@gmail.com>
2024-04-09 13:15:26 +0600

    leedcode database problem 183 solved

---
+++ 4. 183-Customer who never order.sql
@@ -0,0 +1,42 @@
+use db_coding_practice;
+
+CREATE TABLE Customers (
+    id INT PRIMARY KEY auto_increment,
+    name VARCHAR(50)
+);
+
+drop table Customers;
+
+CREATE TABLE Orders (
+    id INT PRIMARY KEY auto_increment,
+    customerId INT,
+    FOREIGN KEY (customerId)
+        REFERENCES Customers (id) 
+);
+
+drop table Orders;
+
+INSERT INTO Customers(name) VALUES 
+('joe'),
+('henry'),
+('sam'),
+('max');
+
+INSERT INTO Orders(customerId) values
+(3),
+(1);
+
+SELECT 
+    *
+FROM
+    Orders;
+    
+SELECT 
+    name AS Customers
+FROM
+    Customers
+        LEFT JOIN
+    Orders ON Customers.id = Orders.customerId
+WHERE
+    Orders.customerId IS NULL;
+
